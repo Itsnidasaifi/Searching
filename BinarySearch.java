1684f55c06ae951eab2a7fe236248c6515ad7c41
Itsnidasaifi <96730205+Itsnidasaifi@users.noreply.github.com>
2023-05-02 19:33:09 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,31 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int arr[]={12,23,45,56,67,78,89};
+        int target=78;
+        int ans=searchingBinary(arr, target);
+        System.out.println("found at index "+ans);
+        
+    }
+    static int searchingBinary(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]<target){
+                start=mid+1;
+
+            }
+            else if(arr[mid]>target){
+                end=mid-1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+
+
+    }
+    
+    
+    
+}
