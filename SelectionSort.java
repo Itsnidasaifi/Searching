80f7bcfdae62c21c64ae16c12e8ce8c76f88c633
Itsnidasaifi <96730205+Itsnidasaifi@users.noreply.github.com>
2023-05-08 20:14:31 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[]={5,4,3,2,1};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+    static void  selection(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            // find the maximum item in the remaining array ans swap  with it
+            int last=arr.length-i-1;
+            int maxIndex = getMaxIndex(arr,0,last); 
+            swap(arr, maxIndex, last);
+            
+
+
+        }
+
+    }
+    static void swap(int arr[],int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+
+
+    }
+    static int getMaxIndex(int arr[],int start,int last){
+        int max=start;
+        for(int i=start;i<=last;i++){
+            if(arr[max]<arr[i]){
+                max=i;
+
+            }
+
+            
+        }
+        return max;
+    }
+    
+}
